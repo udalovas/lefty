20792475e9f30f2f63fb866069102cd6427d838b
udalovas <al.udalec@gmail.com>
2016-07-11 22:49:06 +0100

    Play with React is started

---
+++ assets/js/components/ArticleList.js
@@ -0,0 +1,20 @@
+var ArticleList = React.createClass({
+    render: function () {
+        return <section id="zero" className="wrapper style1">
+            <div className="inner">
+                <Article
+                    title="Title value"
+                    text="Text value"
+                    imgSrc="images/bears/mishka-delovishka_paris.jpg"
+                    moreLinkHref="https://www.instagram.com/levoruchka/" />
+            </div>
+        </section>
+    }
+});
+
+alert('list');
+
+ReactDOM.render(
+    <ArticleList />,
+    document.getElementById('zero')
+);
